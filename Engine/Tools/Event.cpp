348395e9e0c8df82a409009774157f827fa4e779
MunFaill <mainofftv@gmail.com>
2026-06-11 14:57:39 -0300

    Update Event System

--- Engine/Tools/Event.cpp
+++ Engine/Tools/Event.cpp
@@ -1,10 +1,13 @@
 #include "Tools/Event.hpp"
 
+#include <print>
+
 namespace Engine {
     template<class... ArgTypes>
     ListenerID Event<ArgTypes...>::AddListener(Callback callback) {
         ListenerID listener = m_AvaliableListenerID++;
         m_Callbacks.emplace(listener, callback);
+		std::print("New listener added: {}\n", listener);
         return listener;
     }
 
@@ -14,26 +17,29 @@ namespace Engine {
     }
 
 	template<class... ArgTypes>
-	bool Event<ArgTypes...>::RemoveListener(ListenerID listenerID)
+	bool Event<ArgTypes...>::RemoveListener(ListenerID listener)
 	{
-		return m_Callbacks.erase(listenerID) != 0;
+		std::print("Listener removed: {}\n", listener);
+		return m_Callbacks.erase(listener) != 0;
 	}
 
     	template<class... ArgTypes>
-	bool Event<ArgTypes...>::operator-=(ListenerID p_listenerID)
+	bool Event<ArgTypes...>::operator-=(ListenerID listener)
 	{
-		return RemoveListener(p_listenerID);
+		return RemoveListener(listener);
 	}
 
 	template<class... ArgTypes>
 	void Event<ArgTypes...>::RemoveAllListeners()
 	{
+		std::print("Listeners removed\n");
 		m_Callbacks.clear();
 	}
 
 	template<class... ArgTypes>
 	uint64_t Event<ArgTypes...>::GetListenerCount()
 	{
+		std::print("Number of listeners: {}\n", m_Callbacks.size());
 		return m_Callbacks.size();
 	}
 
